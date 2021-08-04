f6c1485ad5ae5d823a748beedf8d12e9c4052024
AQUINRU <russel.aquino@oocl.com>
2021-08-04 14:21:31 +0800

    [PracticeJava][Russel]modified StreamMap to implement 3 functions

--- src/main/java/com/thoughtworks/collection/StreamMap.java
+++ src/main/java/com/thoughtworks/collection/StreamMap.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamMap {
 
@@ -18,14 +20,29 @@ public class StreamMap {
     }
 
     public List<Integer> getTriple(List<Integer> numbers) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return numbers
+                .stream()
+                .map(number -> number * 3)
+                .collect(Collectors.toList());
     }
 
     public List<String> mapLetter(List<Integer> numbers) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        //Function<? super Integer, String> function = number -> ALPHABET.get(number - 1);
+        return numbers
+                .stream()
+                .map(number -> ALPHABET.get(number - 1))
+                .collect(Collectors.toList());
     }
 
     public List<Integer> mapLength(List<String> words) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        //Function<? super String, Integer> function = word -> word.length();
+        Function<? super String, Integer> function = String::length;
+        return words
+                .stream()
+                .map(function)
+                .collect(Collectors.toList());
     }
 }
